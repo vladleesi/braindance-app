5399a02afdf70ed7b5e6cc6c5835880aacdac47c
vladleesi <vladleesi@outlook.com>
2024-02-25 01:45:57 -0600

    Removed test API_KEY

--- shared/src/commonMain/kotlin/dev/vladleesi/braindanceapp/data/config/Config.kt
+++ shared/src/commonMain/kotlin/dev/vladleesi/braindanceapp/data/config/Config.kt
@@ -2,5 +2,5 @@ package dev.vladleesi.braindanceapp.data.config
 
 object Config {
     const val RAWG_HOST = "api.rawg.io/api"
-    const val API_KEY = "f09aca6c5090418d9a8761ab7bca1037"
+    const val API_KEY = ""
 }
