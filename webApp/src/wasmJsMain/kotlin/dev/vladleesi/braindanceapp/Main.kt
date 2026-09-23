a689d0aae0b1f0cd4184d32c356ee24e66521da3
vladleesi <blacoll11@gmail.com>
2026-09-23 04:09:34 -0600

    feat(web): add favicon and improve browser bootstrap

--- webApp/src/wasmJsMain/kotlin/dev/vladleesi/braindanceapp/Main.kt
+++ webApp/src/wasmJsMain/kotlin/dev/vladleesi/braindanceapp/Main.kt
@@ -3,12 +3,11 @@ package dev.vladleesi.braindanceapp
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import dev.vladleesi.braindanceapp.koin.initKoin
-import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    initKoin(debugHttpLogging = true)
-    ComposeViewport(document.body!!) {
+    initKoin()
+    ComposeViewport(viewportContainerId = "webApp") {
         BraindanceApp()
     }
 }
